bcbf5a09c53f913ed1657702c2a6acf3b77c9860
unknown <rojo.angel@gmail.com>
2014-02-22 20:38:09 +0100

    Added plugin skeleton using Flickr sample as a base

--- gplus.lrdevplugin/Info.lua
+++ gplus.lrdevplugin/Info.lua
@@ -3,6 +3,16 @@ return {
 	LrSdkVersion = 3.0,
 	LrSdkMinimumVersion = 3.0, -- minimum SDK version required by this plug-in
 
-	LrToolkitIdentifier = 'com.adobe.lightroom.export.gplus',
+	LrToolkitIdentifier = 'com.rojoa.lightroom.export.gplus',
+    LrPluginName = LOC "$$$/GPlus/PluginName=Google Plus",
 
+    LrExportServiceProvider = {
+        title = LOC "$$$/GPlus/GPlus-title=Google Plus", -- this string appears as the Export destination
+        file = "GPlusExportServiceProvider.lua", -- the service definition script
+        builtInPresetsDir = "gPlusPresets", -- an optional subfolder for presets
+    },
+
+    LrMetadataProvider = 'GPlusMetadataDefinition.lua',
+
+    VERSION = { major=5, minor=0, revision=0, build=907681, },
 }
